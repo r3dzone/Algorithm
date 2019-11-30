9394ad06ad96eb21e7b43925c5778ac3b88b5a87
r3dzone <coolitop123@gmail.com>
2019-11-30 10:25:10 +0000

    C++ STL map use

--- code/map.cpp
+++ code/map.cpp
@@ -6,5 +6,21 @@
 using namespace std;
 
 int main(){
-	map<string, int> m;
+	map<string, int> m1;
+	map<string, double> m2;
+	map<string, string> m3;
+	
+	m1["a"] = 1;
+	m2["b"] = 2.00;
+	m3.insert(make_pair("c","three"));
+
+	cout << "Correct Key use" << "\n";
+	cout << m1.find("a")->second << "\n";
+	cout << m2.find("b")->second << "\n";
+	cout << m3.find("c")->second << "\n";
+
+	cout << "Incorrect Key use" << "\n";
+	cout << m1.find("d")->second << "\n";
+	cout << m2.find("d")->second << "\n";
+	cout << m3.find("d")->second << "\n";
 }
