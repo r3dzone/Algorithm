3b14d60d67a72bb9d188aaad7698ad67f4ded1f5
r3dzone <coolitop123@gmail.com>
2019-12-01 05:51:27 +0000

    C++ STL unique & resize Example

---
+++ code/unique.cpp
@@ -0,0 +1,29 @@
+//Made by R3dzone
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+	vector<int> vec;
+	
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(24);
+	vec.push_back(10);
+	vec.push_back(20);
+	vec.push_back(25);
+	vec.push_back(33);
+	vec.push_back(21);
+	vec.push_back(8);
+	vec.push_back(1);
+	
+	sort(vec.begin(),vec.end());
+	
+	vec.resize(vec.size() - (vec.end() - unique(vec.begin(),vec.end())));
+	
+	for(int i = 0; i < vec.size();i++){
+		cout << vec[i]  << "\n";
+	}
+}
