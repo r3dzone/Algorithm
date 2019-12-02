c6a8878b757b30452950f5d1a864438a6e93a489
r3dzone <coolitop123@gmail.com>
2019-12-02 10:50:41 +0000

    C++ STL numeric_limits Example

---
+++ code/numeric_limits.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main(){
+
+cout << "long long's Max limit : " << numeric_limits<long long>::max() << "\n";
+
+cout << "long long's Min limit : " << numeric_limits<long long>::min() << "\n";
+}
