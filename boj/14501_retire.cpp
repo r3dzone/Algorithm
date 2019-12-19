d814efdd531c50877ba9b6082071fa2da2197d66
r3dzone <coolitop123@gmail.com>
2019-12-19 10:36:27 +0000

    new file

--- boj/14501_retire.cpp
+++ boj/14501_retire.cpp
@@ -4,20 +4,31 @@
 
 using namespace std;
 
-const int MAXN = 1e3;
+const int MAXN = 10000;
 int memo[MAXN+5];
-int arr[MAXN+5];
+int T[MAXN+5];
+int P[MAXN+5];
 
 int n;
 
-int DP(){
+int DP(int x){
+	if(x >= n) return 0;
 	
+	int& ret = memo[x];
+	if(ret != -1) return ret;
+	
+	int tmp = 0;
+	if(T[x] <= n - x){
+		tmp = max(DP(x+T[x])+P[x],DP(x+1));
+		return ret = tmp;
+	}else return ret = DP(x+1);
 }
 
 int main(){
 	memset(memo,-1,sizeof(memo));
 	scanf("%d",&n);
-	for(int i = 0; i < n ; i++)
-		scanf();
-	printf();
+	++n;
+	for(int i = 1; i < n ; i++)
+		scanf("%d %d",&T[i] ,&P[i]);
+	printf("%d\n",DP(1));
 }
