36df20e3fdede236707753ce9fdb3b7ee5eca2db
r3dzone <coolitop123@gmail.com>
2019-12-20 11:04:32 +0000

    new file

--- boj/9461_padovan.cpp
+++ boj/9461_padovan.cpp
@@ -2,22 +2,27 @@
 #include <cstring>
 #include <algorithm>
 
-using namespace std;
+using namespace std; 
 
-const int MAXN = 1e3;
-int memo[MAXN+5];
-int arr[MAXN+5];
+const int MAXN = 100;
+long long memo[MAXN+5];
 
-int n;
+int t,n;
 
-int DP(){
+long long DP(int x){
+	if(x <= 3) return 1;
+	if(x <= 5) return 2;
 	
+	long long& ret = memo[x];
+	if(ret != -1) return ret;
+	return ret = DP(x-1) + DP(x-5);
 }
 
 int main(){
 	memset(memo,-1,sizeof(memo));
-	scanf("%d",&n);
-	for(int i = 0; i < n ; i++)
-		scanf();
-	printf();
+	scanf("%d",&t); 
+	for(int i = 0; i < t ; i++){
+		scanf("%d",&n);
+		printf("%lld\n",DP(n));
+	}
 }
