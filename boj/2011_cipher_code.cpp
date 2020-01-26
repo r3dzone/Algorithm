e0eb543a9a5b95e60578d81bb1552bd56dff0cef
r3dzone <coolitop123@gmail.com>
2020-01-26 08:48:38 +0000

    solve

--- boj/2011_cipher_code.cpp
+++ boj/2011_cipher_code.cpp
@@ -14,11 +14,23 @@ int memo[MAXN];
 char cipher[MAXN];
 
 int DP(int x){
-	if(x > strlen(cipher)) return 0;
-	if(x == strlen(cipher)-1 && cipher[x] >= '1' && cipher[x] <= '9') return 1;
+	if(x >= strlen(cipher)) return 0;
 	
 	int& ret = memo[x];
 	if(ret != -1) return ret;
+	
+	if(x == strlen(cipher)-1){
+		if(cipher[x] >= '1' && cipher[x] <= '9') return ret = 1;
+	}
+	
+	if(x == strlen(cipher)-2){
+		int tmp = 0;
+		if(cipher[x] == '1' && (cipher[x+1] >= '0' && cipher[x+1] <= '9' )) ++tmp;
+		if(cipher[x] == '2' && (cipher[x+1] >= '0' && cipher[x+1] <= '6')) ++tmp;
+		if(cipher[x] >= '1' && cipher[x] <= '9' && cipher[x+1] >= '1' && cipher[x+1] <= '9') ++tmp;
+		if(ret == 2) return ret = 1 + DP(x+1);
+		return ret = tmp;
+	}
 	
 	if(cipher[x] == '1'){
 		if(x+1 <= strlen(cipher) && (cipher[x+1] >= '0' && cipher[x+1] <= '9'))
@@ -35,6 +47,5 @@ int DP(int x){
 int main(){
 	memset(memo,-1,sizeof(memo));
 	scanf("%s",cipher);
-	for(int i = 1 ; i < strlen(cipher); ++i )
-		printf("%d:%d\n",i,DP(i));
+	printf("%d\n",DP(0));
 }
