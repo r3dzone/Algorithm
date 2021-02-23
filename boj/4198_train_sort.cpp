6e164f444db31c9796a401bae852bcf9d394f287
R3dzone <coolitop123@gmail.com>
2021-02-23 19:36:51 +0900

    10% time over

--- boj/4198_train_sort.cpp
+++ boj/4198_train_sort.cpp
@@ -10,6 +10,7 @@ const int MAXN = 2000+5;
 int memoi[MAXN];
 int memod[MAXN];
 int arr[MAXN];
+int tmparr[MAXN];
 
 int n;
 
@@ -19,7 +20,7 @@ int lis(int x){ //x번째 숫자를 시작으로 하는 가장 긴 증가하는 부분수열의 길이
 	
 	int tmp = 0;
 	for(int i = x+1; i < n; i++)
-		if(arr[x] < arr[i])
+		if(tmparr[x] < tmparr[i])
 			tmp = max(lis(i),tmp);
 	return ret = tmp + 1;
 }
@@ -30,9 +31,18 @@ int lds(int x){ //x번째 숫자를 시작으로 하는 가장 긴 감소하는 부분수열의 길이
 	
 	int tmp = 0;
 	for(int i = x+1; i < n; i++)
-		if(arr[x] > arr[i])
+		if(tmparr[x] > tmparr[i])
 			tmp = max(lds(i),tmp);
 	return ret = tmp + 1;
+}
+
+void set(int x){ //x번째 앞은 배열 뒤집어서 x뒤에 붙이고 x뒤 배열은 그 뒤에 붙이기 
+	for(int i = 0; i < n; i++)
+		if(i <= x){
+			tmparr[i] = arr[x-i];
+		}else{
+			tmparr[i] = arr[i];
+		}
 }
 
 int main(){
@@ -41,6 +51,16 @@ int main(){
 	scanf("%d",&n);
 	for(int i = 0; i < n; i++)
 		scanf("%d",&arr[i]);
+	
+	int tmp = 0;
+	for(int i = 0; i < n; i++){
+		memset(memoi,-1,sizeof(memoi));
+		memset(memod,-1,sizeof(memod));
+		set(i);
+		tmp = max(tmp,(lis(i)+lds(i)-1));
+	}
+	printf("%d\n",tmp);
+	/*
 	int tmp = 0;
 	for(int i = 0; i < n; i++)
 		tmp = max(tmp,lis(i));
@@ -53,4 +73,7 @@ int main(){
 	tmp = 0;
 	for(int i = 0; i < n; i++)
 		printf("i:%d)LIS:%d LDS:%d\n",i,lis(i),lds(i));
-}
+
+	*/
+	
+} 
