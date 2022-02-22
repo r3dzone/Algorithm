5696db28ae2f31913fc7f774eb3216f5514af84a
R3dzone <coolitop123@gmail.com>
2022-02-22 10:28:56 +0000

    try

---
+++ trying/5430_ac.cpp
@@ -0,0 +1,111 @@
+//Made By R3dzone
+
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+
+
+using namespace std;
+
+
+
+string p,arr;
+
+int n;
+
+vector<int> vec;
+
+
+
+void print_ans(int x,int y,int rev){ //x부터 y까지 rev == 0 정상출력 || rev == 1 역출력
+
+   
+   
+   }
+   
+   
+   
+   void parse(){
+   
+      int stt = 1;
+	  
+	     for(int i = 1; i < arr.size(); i++){
+		 
+		       if(arr[i] == ','){
+			   
+			            vec.push_back(stoi(arr.substr(stt,i-stt)));
+			//Made By R3dzone
+#include <iostream>
+
+#include <vector>
+
+#include <string>
+
+
+
+using namespace std;
+
+
+
+string p,arr;
+
+int n;
+
+vector<int> vec;
+
+
+
+void print_ans(int x,int y,int rev){ //x부터 y까지 rev == 0 정상출력 || rev == 1 역출력
+   
+
+}
+
+
+
+void parse(){
+
+   int stt = 1;
+
+   for(int i = 1; i < arr.size(); i++){
+
+      if(arr[i] == ','){
+
+         vec.push_back(stoi(arr.substr(stt,i-stt)));
+
+         cout << vec.back() << "\n";
+
+         stt = i+1;
+
+      }
+
+   }
+
+}
+
+
+
+int main(){
+
+   ios::sync_with_stdio(false);
+
+   cin.tie(NULL   
+						         cout << vec.back() << "\n";
+								 
+								          stt = i+1;
+										  
+										        }
+												
+												   }
+												   
+												   }
+												   
+												   
+												   
+												   int main(){
+												   
+												      ios::sync_with_stdio(false);
+													  
+													     cin.tie(NULL
